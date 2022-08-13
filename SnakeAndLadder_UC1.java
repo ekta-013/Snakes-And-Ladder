d9407871a36cee9cd0a1db20aaa23766e1c63af8
ekta <ektamo0123@gmail.com>
2022-08-14 01:34:00 +0530

    Added : first commit

---
+++ SnakeAndLadder_UC1.java
@@ -0,0 +1,10 @@
+package snakesAndLadder;
+
+public class SnakeAndLadder_UC1 {
+
+	public static void main(String[] args) {
+		int playerPosition = 0;
+        System.out.println("Player start at Position : "+playerPosition);		
+
+	}
+}
