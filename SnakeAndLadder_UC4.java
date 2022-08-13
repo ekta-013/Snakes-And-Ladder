88c09a522f5c65037cd09e0777d66e4c21976ffd
ekta <ektamo0123@gmail.com>
2022-08-14 01:49:17 +0530

    Added : winning position

---
+++ SnakeAndLadder_UC4.java
@@ -0,0 +1,34 @@
+package snakesAndLadder;
+
+import java.util.Random;
+
+public class SnakeAndLadder_UC4 {
+    static void winPosition() {
+        int winningPosition = 100;
+        int playerPosition = 0;
+
+        while (playerPosition < winningPosition) {
+
+            Random random = new Random();
+            int rollTheDice = random.nextInt(6)+1;
+            System.out.println("After rolling the dice : "+rollTheDice);
+
+            int optionCheck = random.nextInt(3);
+
+            if (optionCheck == 0) {
+                System.out.println("No play : "+playerPosition);
+            }
+            else if (optionCheck == 1) {
+                playerPosition += rollTheDice;
+                System.out.println("Ladder : "+playerPosition);
+            }
+            else {
+                playerPosition -= rollTheDice;
+                System.out.println("Snake : "+playerPosition);
+            }
+        }
+    }
+	public static void main(String[] args) {
+		 winPosition();
+	}
+}
